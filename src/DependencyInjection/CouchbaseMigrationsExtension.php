0291817c5a2529ab14a8decf1f8ff11e31d55eff
BowlOfSoup <bowlofsoup@boomvos.nl>
2018-03-18 22:11:06 +0100

    Add licence + fix extension.

--- src/DependencyInjection/CouchbaseMigrationsExtension.php
+++ src/DependencyInjection/CouchbaseMigrationsExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class CouchbaseMigrationExtension extends Extension
+class CouchbaseMigrationsExtension extends Extension
 {
     /**
      * @param array $configs

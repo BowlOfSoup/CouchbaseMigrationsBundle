15ca3ae0c7416f2995f834c0e7abbdd143309bd0
Stefan Koopmanschap <stefan.koopmanschap@tenzinger.com>
2024-05-28 15:21:38 +0200

    Load the extension manually

--- src/CouchbaseMigrationsBundle.php
+++ src/CouchbaseMigrationsBundle.php
@@ -2,8 +2,14 @@
 
 namespace BowlOfSoup\CouchbaseMigrationsBundle;
 
+use BowlOfSoup\CouchbaseMigrationsBundle\DependencyInjection\CouchbaseMigrationsExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class CouchbaseMigrationsBundle extends AbstractBundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new CouchbaseMigrationsExtension();
+    }
 }
